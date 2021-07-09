b6d61aa44c0e8bfde6162f0777ef7a167182065d
Mikhail <madenne39@gmail.com>
2021-07-09 17:22:37 +0200

    add db.json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import Card from './Card'
+import Navbar from './Navbar'
 
 function App() {
+  const arr = [1, 2, 3, 4, 5]
   return (
     <div className="App">
-      
+      <div className="container">
+      <Navbar />
+        <div class="row justify-content-md-center">
+          <div class="col-md-auto">
+          <Card  />
+          </div>
+          <div class="col-md-auto">
+          <Card  />
+          </div>
+          <div class="col-md-auto">
+          <Card  />
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+            <div className="navbar-nav">
+              <a className="nav-link active" aria-current="page" href="/">
+                Home
+              </a>
+
+          </div>
+        </div>
+      </nav>
+    </>
+  )
+}
+export default Navbar
